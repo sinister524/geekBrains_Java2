e814d316c8437bfe5000edea977396efe8b90f79
sinister524 <sinister524@gmail.com>
2019-06-09 16:38:10 +0300

    Подготовил ДЗ к 5 Уроку

--- src/ru/geekbrains/java2/Main.java
+++ src/ru/geekbrains/java2/Main.java
@@ -2,7 +2,43 @@ package ru.geekbrains.java2;
 
 public class Main {
 
+    static final int size = 10000000;
+    static final int h = size / 2;
+
     public static void main(String[] args) {
-	// write your code here
+	conversionArr1();
+	conversionArr2();
     }
+
+    public static void conversionArr1 () {
+        float[] arr = new float[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void conversionArr2 () {
+        float[] arr = new float[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        new Thread(new RunnableClass(a1)).start();
+        new Thread(new RunnableClass(a2)).start();
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+
 }
