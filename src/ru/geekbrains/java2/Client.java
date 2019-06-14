02053e67b6eff2c7210313285ea823fd98125f99
sinister524 <sinister524@gmail.com>
2019-06-14 14:04:43 +0300

    Подготовил ДЗ к шестому уроку

--- src/ru/geekbrains/java2/Client.java
+++ src/ru/geekbrains/java2/Client.java
@@ -1,30 +1,25 @@
 package ru.geekbrains.java2;
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Main {
+public class Client {
+
+    private final String SERVER_ADDR = "localhost";
+    private final int SERVER_PORT = 5000;
 
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        Socket socket;
-        try (ServerSocket serverSocket = new ServerSocket(5000)) {
-            System.out.println("Сервер запущен, ожидаем подключения...");
-            socket = serverSocket.accept();
-            System.out.println("Клиент подключился");
+        try {
+            Socket socket = new Socket("localhost", 5000);
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             new Thread(() -> {
                 try {
                     while (socket.isConnected()) {
                         String str = in.readUTF();
-                        if (str.equals("/end")) {
-                            socket.close();
-                            break;
-                        }
-                        System.out.println("Клиент: " + str);
+
+                        System.out.println("Сервер: " + str);
                     }
                 } catch (IOException e){
                     e.printStackTrace();
@@ -32,13 +27,17 @@ public class Main {
             }).start();
             while (socket.isConnected()) {
                 String str = reader.readLine();
-
+                if (str.equals("/end")) {
+                    socket.close();
+                    break;
+                }
                 out.writeUTF(str);
             }
             socket.close();
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
 
     }
+
 }
